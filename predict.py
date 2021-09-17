8b3a1d8128390868a4d455dd08ef587f6e1bdf97
Nguyễn Quyết - Tommy <turtle.ntq@gmail.com>
2021-09-17 13:51:02 +0700

    add

---
+++ predict.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 16 20:27:14 2021
+
+@author: PC
+"""
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+import re
+from vncorenlp import VnCoreNLP
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import pickle
+import numpy as np
+
+num2label = ['affirm_confirm', 'ask_age_wrong', 'ask_date_wrong', 'ask_gender_wrong', 
+             'ask_name_wrong', 'deny_confirm','no_date', 'pick_date',  'provide_age',
+             'provide_gender', 'provide_name']
+
+def normalize_sentence(s):
+    s = re.sub('Ð', 'đ', s)
+    s = s.lower()
+    s = re.sub('2', 'hai', s)
+    s = re.sub('3', 'ba', s)
+    s = re.sub('4', 'tư', s)
+    s = re.sub('5', 'năm', s)
+    s = re.sub('6', 'sáu', s)
+    s = re.sub('7', 'bảy', s)
+    s = re.sub('-', '', s)
+    s = re.sub(',', '', s)
+    s = re.sub("\.", '', s)
+    s = re.sub('\?','', s)
+    s = re.sub('\!','', s)
+    s = re.sub('_','', s)
+    s = s.strip()
+    annotator = VnCoreNLP("VnCoreNLP/VnCoreNLP-1.1.1.jar", annotators="wseg", max_heap_size='-Xmx2g') 
+    annotated_text = annotator.annotate(s)
+    words = annotator.tokenize(s)[0]
+    s = ' '.join(words)  
+    return s
+          
+def padding_sentence(s):
+    with open('tokenizer.pickle', 'rb') as handle:
+         tokenizer = pickle.load(handle)
+    test_sequence = tokenizer.texts_to_sequences([s])
+    padded_test_sequences = pad_sequences(test_sequence, maxlen=20, truncating="post", padding="post")
+    return padded_test_sequences
+
+class NLPModel:
+    def __init__(self, sentence):
+        self.sentence = sentence
+        self.model = load_model("NLPModel.h5")
+    
+    def predict(self):
+        normal_sentence = normalize_sentence(self.sentence)
+        padded_sentence = padding_sentence(normal_sentence)
+        prediction = self.model.predict(padded_sentence)
+        p = round(np.max(prediction, axis=1)[0],2)
+        if p > 0.5:
+            label = num2label[np.argmax(prediction, axis = 1)[0]]
+        else:
+            label = '_'
+        return (label, p)
+    
+if __name__ == "__main__":
+   model = NLPModel('mình giới_tính nam') 
+   (label, p) = model.predict()
+           
